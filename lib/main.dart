20f335d0f7276144a878aaa3d5e5660a378af569
Krish Shrestha <103551556+krishrestha@users.noreply.github.com>
2024-07-12 22:24:54 +0545

    calculator

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Calculator',
+      theme: ThemeData.dark(),
+      home: CalculatorHomePage(),
+    );
+  }
+}
+
+class CalculatorHomePage extends StatefulWidget {
+  @override
+  _CalculatorHomePageState createState() => _CalculatorHomePageState();
+}
+
+class _CalculatorHomePageState extends State<CalculatorHomePage> {
+  String _output = "0";
+  String _input = "";
+  double _num1 = 0;
+  double _num2 = 0;
+  String _operator = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _input = "";
+      _num1 = 0;
+      _num2 = 0;
+      _operator = "";
+      _output = "0";
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "×" || buttonText == "÷") {
+      _num1 = double.parse(_input);
+      _operator = buttonText;
+      _input = "";
+    } else if (buttonText == ".") {
+      if (!_input.contains(".")) {
+        _input = _input + buttonText;
+      }
+    } else if (buttonText == "=") {
+      _num2 = double.parse(_input);
+
+      if (_operator == "+") {
+        _output = (_num1 + _num2).toString();
+      }
+      if (_operator == "-") {
+        _output = (_num1 - _num2).toString();
+      }
+      if (_operator == "×") {
+        _output = (_num1 * _num2).toString();
+      }
+      if (_operator == "÷") {
+        _output = (_num1 / _num2).toString();
+      }
+
+      _num1 = 0;
+      _num2 = 0;
+      _operator = "";
+      _input = _output;
+    } else {
+      _input = _input + buttonText;
+      _output = _input;
+    }
+
+    setState(() {
+      _output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 12.0,
+            ),
+            child: Text(
+              _output,
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  buildButton("7"),
+                  buildButton("8"),
+                  buildButton("9"),
+                  buildButton("÷"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("4"),
+                  buildButton("5"),
+                  buildButton("6"),
+                  buildButton("×"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("1"),
+                  buildButton("2"),
+                  buildButton("3"),
+                  buildButton("-"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("."),
+                  buildButton("0"),
+                  buildButton("00"),
+                  buildButton("+"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("C"),
+                  buildButton("="),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
